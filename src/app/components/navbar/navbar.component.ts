a43bd8fee727e440412547f46f7ec9b7247e07ac
ginocannata <ginoca22@gmail.com>
2023-04-20 12:29:44 -0300

    Fix de boton en vista mobile

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +11,11 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $(document).ready(function(){
+      $(".navbar-toggler").click(function(){
+        $(".navbar-collapse").toggle();
+      });
+    });
   }
 
 }
